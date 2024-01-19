4b587b5f097cc747492e5ad22a89a0e98c4f85c6
KaruzG <josemiguelcpcs@gmail.com>
2024-01-19 12:47:03 +0100

    feat(dropdown): SHow/hide functionality, needs fix

--- components/chirpHeader.js
+++ components/chirpHeader.js
@@ -4,10 +4,14 @@
 // vars.css
 // header.css
 
+// ISSUES
+// EL constructor se ejecuta 2 veces 
+
 class ChirpHeader extends HTMLElement {
     constructor() {
         super()
         this.render()
+        this.dropdownButton()
     }
 
     render() {
@@ -19,18 +23,56 @@ class ChirpHeader extends HTMLElement {
         outerContainer.appendChild(logo)
 
         let button = document.createElement("button")
-        button.id = "headerDropdown"
+        button.id = "headerDropdownButton"
         outerContainer.appendChild(button)
 
         let buttonSvg = document.createElement("img")
         buttonSvg.src = "../public/svg/icons8-menu-bar.svg"
         button.appendChild(buttonSvg)
 
+        // Dropwdown
+        let dropdown = document.createElement("div")
+        dropdown.id = "headerDropdownOuter"
+
+        outerContainer.appendChild(dropdown)
+
         this.appendChild(outerContainer)
     }
 
     dropdownButton() {
-        
+        if (document.body.contains(document.getElementById("ulDropdown"))) {
+            return false
+        }
+
+        let button = document.getElementById("headerDropdownButton")
+        let dropdown = document.getElementById("headerDropdownOuter")
+
+        // TODO: img Usuario y usuario
+        // ...
+
+        // Lista del dropdown
+        let ul = document.createElement("ul")
+        ul.id = "ulDropdown"
+
+        const dropdownList = ["Home", "Saved", "Profile", "Settings"]
+        for (const i of dropdownList) {
+            ul.appendChild(document.createElement("hr"))
+            let li = document.createElement("li")
+            li.innerText = i
+            ul.appendChild(li)
+        }
+        dropdown.append(ul)
+
+
+        // Ocultar/Mostrar dropdown
+        button.addEventListener("click", (ev) => {
+            ev.stopImmediatePropagation()
+            if (dropdown.style.display === "block") {
+                dropdown.style.display = "none"
+            } else {
+                dropdown.style.display = "block"
+            }
+        })
     }
 }
 
