1a1264d03c58dee3e611b664acdd53717e078c45
Jaume <jaume@abastos.org>
2024-01-29 13:29:49 +0100

    Rama actualizada

--- config.php
+++ config.php
@@ -1,3 +1,17 @@
-<!-- CONFIGURATION FILE FOR CHIRP APP -->
+<?php
+// CONFIGURATION FILE FOR CHIRP APP
 
-<!-- DATABASE CONFIG -->
+    // APP CONFIG
+        define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT']);
+
+
+    // DATABASE CONFIG
+
+        // DATABASE LOCATION
+        define('SERVERNAME', "localhost");
+
+        // USERNAME and PASSWORD
+        define('USERNAME', "root");
+        define('PASSWORD', "password");
+
+?>
