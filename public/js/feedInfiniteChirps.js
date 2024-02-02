4fc804e2688d049c817668daae25aa293df73321
KaruzG <josemiguelcpcs@gmail.com>
2024-02-02 18:52:05 +0100

    wip infinite scroll

--- public/js/feedInfiniteChirps.js
+++ public/js/feedInfiniteChirps.js
@@ -1,14 +1,15 @@
 function infiniteScroll() {
 
     document.addEventListener('scroll', function(e) {
-        console.log("a");
         let documentHeight = document.body.scrollHeight;
         let currentScroll = window.scrollY + window.innerHeight;
-        // When the user is [modifier]px from the bottom, fire the event.
-        let modifier = 10; 
-        if(currentScroll + modifier > documentHeight) {
-            console.log('You are at the bottom!')
-            console.log(currentScroll, documentHeight);
+        console.log(currentScroll);
+        console.log(documentHeight);
+        if(currentScroll == documentHeight) {
+            let chirp = document.createElement("chirp-box");
+            chirp.setAttribute("chirpId", 1);
+
+            document.body.appendChild(chirp);
         }
     })
 
