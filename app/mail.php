72aedae6d2c93ccc3af18dc48be6f8b30d14c8d1
KaruzG <josemiguelcpcs@gmail.com>
2024-02-07 20:06:48 +0100

    feat(ChirpMail): New controller for mails

--- app/mail.php
+++ app/mail.php
@@ -6,32 +6,38 @@ require $_SERVER['DOCUMENT_ROOT'].'/app/lib/PHPMailer/src/Exception.php';
 require $_SERVER['DOCUMENT_ROOT'].'/app/lib/PHPMailer/src/PHPMailer.php';
 require $_SERVER['DOCUMENT_ROOT'].'/app/lib/PHPMailer/src/SMTP.php';
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+include_once $_SERVER['DOCUMENT_ROOT'].'/config.php';
 
-try {
-    //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com.';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'chirpSocial001@gmail.com';                     //SMTP username
-    $mail->Password   = 'CreUnaClaSeg';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+class ChirpMail {
+    function __construct() {
+        $this->mail = new PHPMailer(true);
+    }
 
-    //Recipients
-    $mail->setFrom('chirpSocial001@gmail.com', 'chirpSocial001');
-    $mail->addAddress('josemiguelcpcs@gmail.com');               //Name is optional
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    public function send($sendTo, $subject, $body) {
+        try {
+            $this->mail->isSMTP();                                              //Send using SMTP
+            $this->mail->Host       = SMTP_HOST;                                //Set the SMTP server to send through
+            $this->mail->SMTPAuth   = true;                                     //Enable SMTP authentication
+            $this->mail->Username   = SMTP_USER;                                //SMTP username
+            $this->mail->Password   = SMTP_PASSWORD;                            //SMTP password
+            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;           //Enable implicit TLS encryption
+            $this->mail->Port       = 587;                                      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        
+            //Recipients
+            $this->mail->setFrom(SMTP_USER, MAIL_NAME);
+            $this->mail->addAddress($sendTo);                  //Name is optional
+        
+            //Content
+            $this->mail->isHTML(true);                                          //Set email format to HTML
+            $this->mail->Subject = $subject;
+            $this->mail->Body    = $body;
+        
+            $this->mail->send();
+            header("HTTP/1.0 200 Success");
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            header("HTTP/1.0 500 Internal server error");
+            echo "Message could not be sent. Mailer Error: {$this->mail->ErrorInfo}";
+        }
+    }
 }
